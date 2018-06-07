5c35b3ab99ac584281b82a173c14467a9bae3952
Warryz <8317771+Warryz@users.noreply.github.com>
2018-06-07 16:21:52 +0200

    Created models for article, warehouse, shelf and a storageunit.

--- pywarehouse/models.py
+++ pywarehouse/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    name = models.CharField(max_length=40)
+    amount = models.IntegerField()
+    article_numer = models.BigIntegerField(unique=True)
+
+
+class Warehouse(models.Model):
+    name = models.CharField(max_length=30)
+    warehouse_number = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name='inWarehouse')
+
+
+class Shelf(models.Model):
+    shelf_number = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name='inShelf')
+
+
+class StorageUnit(models.Model):
+    storageunit = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name='inStorageUnit')
