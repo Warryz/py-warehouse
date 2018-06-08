f974e56534ba0986536ea5adb4a9762bb50faee9
Warryz <8317771+Warryz@users.noreply.github.com>
2018-06-08 07:37:50 +0200

    Added test to verifty the right function when opening the main page.

--- pywarehouse/tests.py
+++ pywarehouse/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import resolve, reverse
+
+from .views import home
+
 # Create your tests here.
 
 
@@ -8,3 +11,7 @@ class HomeTests(TestCase):
         url = reverse('home')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, home)
