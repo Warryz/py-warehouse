c09d6ef406ee6ef0b4bcab09a4af7ca6199c5ee4
Warryz <8317771+Warryz@users.noreply.github.com>
2018-06-12 21:44:52 +0200

    Added tests for the article overview page.

--- pywarehouse/tests.py
+++ pywarehouse/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
 
-from .views import home
+from .views import home, article_overview
 
 # Create your tests here.
 
@@ -15,3 +15,21 @@ class HomeTests(TestCase):
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEqual(view.func, home)
+
+
+class ArticleOverviewTests(TestCase):
+    """Tests the article_overview page.
+    """
+
+    def test_http_return_code_article_view(self):
+        """Test checks for the right http
+        return code 200."""
+        url = reverse('article_overview')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_article_url_resolves_article_view(self):
+        """Tests verifies if the /articles/ url
+        resolves the article_overview"""
+        view = resolve('/articles/')
+        self.assertEqual(view.func, article_overview)
